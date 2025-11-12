1af6b16554bdfb3f315754c3c797aef3ff648c77
Nelson <sosafco3@gmail.com>
2025-11-11 21:51:17 -0600

    Creación de la pantalla 'Login'

--- sistema-buap-webapp/src/app/app.module.ts
+++ sistema-buap-webapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
 
-// Angular Material
+// Elementos De Angular Material
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
@@ -24,7 +25,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatIconModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- sistema-buap-webapp/src/app/screens/login-screen/login-screen.component.ts
+++ sistema-buap-webapp/src/app/screens/login-screen/login-screen.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnInit {
+  public type: string = 'password';
+  public username: string = '';
+  public password: string = '';
+  public isLoading: boolean = false;
+  public errors: any = {};
 
+  ngOnInit(): void {
+
+  }
+
+  public login() {
+
+  }
+
+  public showPassword() {
+
+  }
+
+  public registrar() {
+  }
 }
