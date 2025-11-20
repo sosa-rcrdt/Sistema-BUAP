befa49fd2f90d0b88f88b0bfe85edaafbe589833
Nelson <sosafco3@gmail.com>
2025-11-19 21:44:00 -0600

    Formulario De Registro - Alumnos

--- sistema-buap-webapp/src/app/partials/registro-alumnos/registro-alumnos.component.ts
+++ sistema-buap-webapp/src/app/partials/registro-alumnos/registro-alumnos.component.ts
@@ -1,12 +1,99 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+declare var $:any;
 
 @Component({
   selector: 'app-registro-alumnos',
   templateUrl: './registro-alumnos.component.html',
   styleUrls: ['./registro-alumnos.component.scss']
 })
-export class RegistroAlumnosComponent {
+export class RegistroAlumnosComponent implements OnInit{
   @Input() rol: string = "";
   @Input() datos_user: any = {};
+  //Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+
+  public alumno:any= {};
+  public token: string = "";
+  public errors:any={};
+  public editar:boolean = false;
+  public idUser: Number = 0;
+
+  constructor(
+    private router: Router,
+    private location : Location,
+    public activatedRoute: ActivatedRoute,
+    private alumnosService: AlumnosService,
+  ){}
+
+  ngOnInit(): void {
+    this.alumno = this.alumnosService.esquemaAlumno();
+    this.alumno.rol = this.rol;
+  }
+
+  public regresar(){
+    this.location.back();
+  }
+
+  //Funciones para password
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  showPwdConfirmar()
+  {
+    if(this.inputType_2 == 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
+  }
+
+  public registrar(){
+    //Validar
+    this.errors = [];
+
+    this.errors = this.alumnosService.validarAlumno(this.alumno, this.editar);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+    //Validar la contraseña
+    if(this.alumno.password == this.alumno.confirmar_password){
+      //Aquí si todo es correcto vamos a registrar - aquí se manda a llamar al servicio
+    }else{
+      alert("Las contraseñas no coinciden");
+      this.alumno.password="";
+      this.alumno.confirmar_password="";
+    }
+  }
+
+  public actualizar(){
+
+  }
+
+  //Función para detectar el cambio de fecha
+  public changeFecha(event :any){
+    console.log(event);
+    console.log(event.value.toISOString());
+
+    this.alumno.fecha_nacimiento = event.value.toISOString().split("T")[0];
+    console.log("Fecha: ", this.alumno.fecha_nacimiento);
+  }
 
 }
